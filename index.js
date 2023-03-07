f911dc56b42a37c76383fb57cd8186c6d43ea14c
whole-earth <67808476+whole-earth@users.noreply.github.com>
2023-03-07 09:45:51 -0800

    Update index.js

--- index.js
+++ index.js
@@ -232,9 +232,9 @@ window.addEventListener('load', async () => {
     const cover = document.querySelector('.intro-wrap');
     const content = document.querySelector('.content');
     const startScale = window.innerWidth < 480 ? 0.6 : window.innerWidth < 768 ? 0.8 : 1.3; // ternary
-    const endPoint = 180;
-    const endScale = window.innerWidth < 768 ? 4 : 6;
-    const opacityTransLength = 120;
+    const endPoint = window.innerWidth < 768 ? 100 : 180; // ternary
+    const endScale = window.innerWidth < 768 ? 4 : 6; // ternary
+    const opacityTransLength = window.innerWidth < 768 ? 80 : 120; // ternary
     const opacityTransMark = endPoint - opacityTransLength;
 
     content.style.opacity = 0;
