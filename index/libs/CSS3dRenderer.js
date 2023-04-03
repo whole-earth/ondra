35e8a8f5f2805297e83d46ee457234c01822a58d
whole-earth <67808476+whole-earth@users.noreply.github.com>
2023-04-02 18:49:36 -0700

    Update CSS3dRenderer.js

--- index/libs/CSS3dRenderer.js
+++ index/libs/CSS3dRenderer.js
@@ -1,7 +1,7 @@
 import {
     Matrix4,
     Object3D
-} from './threejs/three.module.js';
+} from './three.module.js';
 
 /**
  * Based on http://www.emagix.net/academic/mscs-project/item/camera-sync-with-css3-and-webgl-threejs
