bd933f1c69fe27d0c95c21f5383e1c03ffab7f15
whole-earth <67808476+whole-earth@users.noreply.github.com>
2023-05-30 22:47:40 -0700

    Update photoDisplay.js

--- index/photoDisplay.js
+++ index/photoDisplay.js
@@ -81,7 +81,7 @@ function defineHelixTransform(objects, vector) {
         if (window.innerWidth > 768) {
             distance = 2000;
         } else {
-            distance = 1400; // mobile
+            distance = 1700; // mobile
         }
 
         const object = new THREE.Object3D();
