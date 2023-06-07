5f897d4f5f18cfc2500a9a9ce6045685807fc156
Ondra <67808476+whole-earth@users.noreply.github.com>
2023-06-07 16:20:01 -0700

    Update cover animation

--- index/main.js
+++ index/main.js
@@ -107,7 +107,7 @@ window.addEventListener('load', async () => {
 
     document.querySelector('.intro-wrap').style.pointerEvents = 'none';
 
-    await delay(1000);
+    await delay(1800); // waits until end of navExpand()
     researchAnimate();
     await waitUntil(() => !researchIsAnimating);
 
