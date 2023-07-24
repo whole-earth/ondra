6340ca011f541d0ad0a6ebf60a8b54ca0f062c95
Ondra <67808476+whole-earth@users.noreply.github.com>
2023-07-24 10:02:35 -0700

    Update cursorAnim.js

--- hire/cursorAnim.js
+++ hire/cursorAnim.js
@@ -16,68 +16,75 @@ if (isMobile()) {
   // Global variables
   var trail = [];
   var h = 0;
+  var drawingEnabled = true; // Flag to control drawing
+  var clearTrail = true;
 
   function setup() {
-    createCanvas(windowWidth, windowHeight);
+    // createCanvas(1440, 900);
+    var canvas = createCanvas(windowWidth, document.body.offsetHeight);
+    canvas.parent('cursor-anim');
     colorMode(HSB);
     noFill();
-    strokeWeight(3); // Adjust the thickness of the line
+    strokeWeight(4); // Adjust the thickness of the line
+
   }
 
-  function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
-  }
-
-  function mouseMoved() {
-    trail.push({
-      'x': mouseX,
-      'y': mouseY
-    });
-    if (trail.length > trailLength) {
-      trail.splice(0, trail.length - trailLength);
-    }
+  function doubleClicked() {
+    toggleDrawing();
+    console.log("Cursor animation = " + drawingEnabled);
   }
 
   function draw() {
     background(255);
-    var thisColor = h;
-    beginShape();
-    for (var i = 1; i < trail.length - 2; i++) {
-      var seg0 = trail[i - 1];
-      var seg1 = trail[i];
-      var seg2 = trail[i + 1];
-      var seg3 = trail[i + 2];
-      stroke(thisColor, 100, 100);
-      drawCatmullRom(seg0, seg1, seg2, seg3);
-      thisColor = (thisColor + innerColorSpeed) % 360;
+    // Check if drawing is enabled
+    if (drawingEnabled) {
+      trail.push({
+        'x': mouseX,
+        'y': mouseY
+      });
+
+      var thisColor = h;
+      for (var i = 1; i < trail.length - 2; i++) {
+        var seg0 = trail[i - 1];
+        var seg1 = trail[i];
+        var seg2 = trail[i + 1];
+        var seg3 = trail[i + 2];
+        stroke(thisColor, 100, 100);
+        drawCatmullRom(seg0, seg1, seg2, seg3);
+        thisColor = (thisColor + innerColorSpeed) % 360;
+      }
+
+      if (trail.length > trailLength) {
+        trail.splice(0, max(trail.length - trailLength, 0));
+      }
+
+      h = (h + colorSpeed) % 360;
     }
-    endShape();
-    h = (h + colorSpeed) % 360;
+
+    function drawCatmullRom(p0, p1, p2, p3) {
+      var amount = 0.05; // Adjust this value for the smoothness
+      for (var t = 0; t < 1; t += amount) {
+        var x = 0.5 * ((2 * p1.x) +
+          (-p0.x + p2.x) * t +
+          (2 * p0.x - 5 * p1.x + 4 * p2.x - p3.x) * t * t +
+          (-p0.x + 3 * p1.x - 3 * p2.x + p3.x) * t * t * t);
+        var y = 0.5 * ((2 * p1.y) +
+          (-p0.y + p2.y) * t +
+          (2 * p0.y - 5 * p1.y + 4 * p2.y - p3.y) * t * t +
+          (-p0.y + 3 * p1.y - 3 * p2.y + p3.y) * t * t * t);
+        var px = 0.5 * ((2 * p1.x) +
+          (-p0.x + p2.x) * (t + amount) +
+          (2 * p0.x - 5 * p1.x + 4 * p2.x - p3.x) * (t + amount) * (t + amount) +
+          (-p0.x + 3 * p1.x - 3 * p2.x + p3.x) * (t + amount) * (t + amount) * (t + amount));
+        var py = 0.5 * ((2 * p1.y) +
+          (-p0.y + p2.y) * (t + amount) +
+          (2 * p0.y - 5 * p1.y + 4 * p2.y - p3.y) * (t + amount) * (t + amount) +
+          (-p0.y + 3 * p1.y - 3 * p2.y + p3.y) * (t + amount) * (t + amount) * (t + amount));
+        line(x, y, px, py);
+      }
+    }
   }
 
-  function drawCatmullRom(p0, p1, p2, p3) {
-    var amount = 0.05; // Adjust this value for the smoothness
-    for (var t = 0; t < 1; t += amount) {
-      var x = 0.5 * ((2 * p1.x) +
-        (-p0.x + p2.x) * t +
-        (2 * p0.x - 5 * p1.x + 4 * p2.x - p3.x) * t * t +
-        (-p0.x + 3 * p1.x - 3 * p2.x + p3.x) * t * t * t);
-      var y = 0.5 * ((2 * p1.y) +
-        (-p0.y + p2.y) * t +
-        (2 * p0.y - 5 * p1.y + 4 * p2.y - p3.y) * t * t +
-        (-p0.y + 3 * p1.y - 3 * p2.y + p3.y) * t * t * t);
-      var px = 0.5 * ((2 * p1.x) +
-        (-p0.x + p2.x) * (t + amount) +
-        (2 * p0.x - 5 * p1.x + 4 * p2.x - p3.x) * (t + amount) * (t + amount) +
-        (-p0.x + 3 * p1.x - 3 * p2.x + p3.x) * (t + amount) * (t + amount) * (t + amount));
-      var py = 0.5 * ((2 * p1.y) +
-        (-p0.y + p2.y) * (t + amount) +
-        (2 * p0.y - 5 * p1.y + 4 * p2.y - p3.y) * (t + amount) * (t + amount) +
-        (-p0.y + 3 * p1.y - 3 * p2.y + p3.y) * (t + amount) * (t + amount) * (t + amount));
-      vertex(x, y);
-    }
-  }
-  
   function toggleDrawing() {
     // Function to toggle drawing on double-click
     if (drawingEnabled) {
