ccb2a080a9ba12b166bc9b1fcf2661b886e6aa66
Ondra <67808476+whole-earth@users.noreply.github.com>
2023-08-12 15:33:10 -0700

    update grid border-colors

--- hire/hire.js
+++ hire/hire.js
@@ -273,9 +273,9 @@ function initPeace() {
 
           cell.style.backgroundColor = cellState ? "black" : "inherit";
           if (window.innerWidth >= 768) {
-            cell.style.borderColor = cellState ? "black" : "#c7c7c736";
+            cell.style.borderColor = cellState ? "black" : "#cccccc45";
           } else {
-            cell.style.borderColor = cellState ? "black" : "#0000001f";
+            cell.style.borderColor = cellState ? "black" : "#cccccc45";
           }
 
         }
@@ -358,9 +358,9 @@ function initCDJ() {
             cell.style.backgroundColor = cellState ? "black" : "inherit";
 
             if (window.innerWidth >= 768) {
-              cell.style.borderColor = cellState ? "black" : "#c7c7c736";
+              cell.style.borderColor = cellState ? "black" : "#cccccc45";
             } else {
-              cell.style.borderColor = cellState ? "black" : "#0000001f";
+              cell.style.borderColor = cellState ? "black" : "#cccccc45";
             }
           }
         }
