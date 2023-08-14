1b974fba31b31aeabbbc0d4b2bdce6d844ac5a91
Ondra <67808476+whole-earth@users.noreply.github.com>
2023-08-14 10:24:16 -0700

    gaia

--- campus/earthwise/gaia.js
+++ campus/earthwise/gaia.js
@@ -1,1 +1,18 @@
-console.log('Propagated')
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Propagated");
+    favicon();
+});
+
+function favicon() {
+    const favicon32 = document.head.querySelector('link[rel="shortcut icon"]');
+    const favicon256 = document.head.querySelector('link[rel="apple-touch-icon"]');
+    
+    if (favicon32) {
+        favicon32.href = 'https://uploads-ssl.webflow.com/61cbb439e6b4856c4aea9529/6475569a90745d54a119a184_win-favicon-32.png';
+    }
+
+    if (favicon256) {
+        favicon256.href = 'https://uploads-ssl.webflow.com/61cbb439e6b4856c4aea9529/647553e954d0e02cef92e400_win-favicon-256.png';
+    }
+
+}
